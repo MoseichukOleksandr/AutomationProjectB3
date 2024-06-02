74d5b8224c2b77c91807bd781da5d2dc113c8326
MoseichukOleksandr <moseichuk.al@gmail.com>
2024-06-02 13:47:23 -0400

    My first commit

--- src/test/java/io/loop/test/da1_selenium_intro/SeleniumTest.java
+++ src/test/java/io/loop/test/da1_selenium_intro/SeleniumTest.java
@@ -3,6 +3,7 @@ package io.loop.test.da1_selenium_intro;
 public class SeleniumTest {
     public static void main(String[] args) {
         System.out.println("Hello GitHub");
+        System.out.println("Hello GitHub --- Develop branch is here");
     }
 
 
