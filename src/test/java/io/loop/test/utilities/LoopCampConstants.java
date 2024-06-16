03db01ec37f1911e813686ea300434ff0f513b64
MoseichukOleksandr <moseichuk.al@gmail.com>
2024-06-16 11:32:43 -0400

    My first commit

--- src/test/java/io/loop/test/utilities/LoopCampConstants.java
+++ src/test/java/io/loop/test/utilities/LoopCampConstants.java
@@ -4,5 +4,4 @@ public class LoopCampConstants {
 
     public static final String EXPECTED_HEADER_FOR_FORM = "Registration form";
     public static final String EXPECTED_PLACEHOLDER_FIRSTNAME = "first name";
-
 }
