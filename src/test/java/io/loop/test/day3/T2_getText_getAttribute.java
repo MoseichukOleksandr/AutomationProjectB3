380a60541b222873ca7188efae8669e1f9e2f4fc
MoseichukOleksandr <moseichuk.al@gmail.com>
2024-06-16 10:52:35 -0400

    My first commit

--- src/test/java/io/loop/test/day3/T2_getText_getAttribute.java
+++ src/test/java/io/loop/test/day3/T2_getText_getAttribute.java
@@ -1,7 +1,7 @@
 package io.loop.test.day3;
 
+import io.loop.test.utilities.LoopCampConstants;
 import io.loop.test.utilities.WebDriverUtil;
-import org.bouncycastle.asn1.gm.GMObjectIdentifiers;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -23,14 +23,26 @@ public class T2_getText_getAttribute {
         WebElement headerForForm = driver.findElement(By.tagName("h2"));
         String actualHeaderForForm = headerForForm.getText();
 
-        if(actualHeaderForForm.equals(LoopCampConstants.EXPECTED_HEADER_FOR_FORM)){
+        if (actualHeaderForForm.equals(LoopCampConstants.EXPECTED_HEADER_FOR_FORM)) {
             System.out.println("Expected header for form: \"" + LoopCampConstants.EXPECTED_HEADER_FOR_FORM + "\", matches actual header: \"" + actualHeaderForForm + "\" => TEST PASSED");
         } else {
             System.err.println("Expected header for form: \"" + LoopCampConstants.EXPECTED_HEADER_FOR_FORM + "\", DOES NOT MATCH actual header: \"" + actualHeaderForForm + "\" => TEST FAILED");
         }
+
+        WebElement firstNamePlaceHolder = driver.findElement(By.name("firstname"));
+        String actuaPlaceHolderForFirstName = firstNamePlaceHolder.getAttribute("placeholder");
+
+        if (actuaPlaceHolderForFirstName.equals(LoopCampConstants.EXPECTED_PLACEHOLDER_FIRSTNAME)) {
+            System.out.println("Expected placeholder matches with actual ");
+            System.out.println("TEST PASSED");
+        } else {
+            System.out.println("Expected placeholder are DOES NOT MAtch  with actual");
+            System.err.println("TEST FAILED");
+        }
+
+
+
+
+
     }
-
-
-
-
 }
